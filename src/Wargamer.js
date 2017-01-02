0c715e26ae4dc968212e26a01e62360737892b6a
Kelvin Wu <kelvinwu1996@gmail.com>
2017-01-01 20:01:26 -0500

    Add exports.

---
+++ src/Wargamer.js
@@ -0,0 +1,71 @@
+import WorldOfTanks from './clients/WorldOfTanks';
+import WorldOfTanksBlitz from './clients/WorldOfTanksBlitz';
+import WorldOfTanksConsole from './clients/WorldOfTanksConsole';
+import WorldOfWarships from './clients/WorldOfWarships';
+import WorldOfWarplanes from './clients/WorldOfWarplanes';
+import Wargaming from './clients/Wargaming';
+
+/**
+ * @classdesc The Wargamer client.
+ */
+export default class Wargamer {
+  /**
+   * Constructs a new World of Tanks API client.
+   * @param {ClientOptions} options - The client options.
+   * @returns {WorldOfTanks} The API client.
+   * @static
+   */
+  static WoT(options) {
+    return new WorldOfTanks(options);
+  }
+
+  /**
+   * Constructs a new World of Tanks Blitz API client.
+   * @param {ClientOptions} options - The client options.
+   * @returns {WorldOfTanksBlitz} The API client.
+   * @static
+   */
+  static WoTB(options) {
+    return new WorldOfTanksBlitz(options);
+  }
+
+  /**
+   * Constructs a new World of Tanks Console API client.
+   * @param {ClientOptions} options - The client options.
+   * @returns {WorldOfTanksConsole} The API client.
+   * @static
+   */
+  static WoTC(options) {
+    return new WorldOfTanksConsole(options);
+  }
+
+  /**
+   * Constructs a new World of Warships API client.
+   * @param {ClientOptions} options - The client options.
+   * @returns {WorldOfWarships} The API client.
+   * @static
+   */
+  static WoWS(options) {
+    return new WorldOfWarships(options);
+  }
+
+  /**
+   * Constructs a new World of Warplanes API client.
+   * @param {ClientOptions} options - The client options.
+   * @returns {WorldOfWarplanes} The API client.
+   * @static
+   */
+  static WoWP(options) {
+    return new WorldOfWarplanes(options);
+  }
+
+  /**
+   * Constructs a new Wargaming.net API client.
+   * @param {ClientOptions} options - The client options.
+   * @returns {Wargaming} The API client.
+   * @static
+   */
+  static WGN(options) {
+    return new Wargaming(options);
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import Wargamer from './Wargamer';
+import WorldOfTanks from './clients/WorldOfTanks';
+import WorldOfTanksBlitz from './clients/WorldOfTanksBlitz';
+import WorldOfTanksConsole from './clients/WorldOfTanksConsole';
+import WorldOfWarships from './clients/WorldOfWarships';
+import WorldOfWarplanes from './clients/WorldOfWarplanes';
+import Wargaming from './clients/Wargaming';
+
+export {
+  WorldOfTanks,
+  WorldOfTanksBlitz,
+  WorldOfTanksConsole,
+  WorldOfWarships,
+  WorldOfWarplanes,
+  Wargaming,
+};
+
+export default Wargamer;
