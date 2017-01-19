3ef29bdf0d3e1c4499133a27b4d1e169774d55a3
Kelvin Wu <kelvinwu1996@gmail.com>
2017-01-18 22:16:48 -0500

    Trim base module code.

--- src/modules/ClientModule.js
+++ src/modules/ClientModule.js
@@ -1,29 +1,13 @@
-import CacheManager from '../cache/CacheManager';
-
 /**
  * @classdesc A module within an API client.
  */
 class ClientModule {
   /**
-   * The module cache manager.
-   * @type {CacheManager}
-   * @static
-   * @private
-   */
-  static caches = new CacheManager();
-
-  /**
-   * The default time to live for cache contents.
-   * @type {number}
-   */
-  DEFAULT_CACHE_TTL = 3600 * 1000;
-
-  /**
    * Constructor.
    * @param {BaseClient} client - The API client this module belongs to.
-   * @param {string} moduleName - The name of the module.
+   * @param {string} name - The name of the module.
    */
-  constructor(client, moduleName) {
+  constructor(client, name) {
     /**
      * The API client this module belongs to.
      * @type {BaseClient}
@@ -34,50 +18,7 @@ class ClientModule {
      * The name of the module.
      * @type {string}
      */
-    this.moduleName = moduleName;
-  }
-
-  /**
-   * The unique cache identifier prefix for this module.
-   * @type {string}
-   */
-  get cachePrefix() {
-    return `${this.client.type}:${this.moduleName}`;
-  }
-
-  /**
-   * Gets a module cache.
-   * @param {string} name - The name of the cache to get.
-   * @returns {(Cache|undefined)} The cache if found, otherwise `undefined`.
-   */
-  getCache(name) {
-    return this.constructor.caches.get(`${this.cachePrefix}:${name}`);
-  }
-
-  /**
-   * Checks if a module cache exists.
-   * @param {string} name - The name of the cache.
-   * @returns {boolean} True if the cache exists, else false.
-   */
-  hasCache(name) {
-    return this.constructor.caches.has(`${this.cachePrefix}:${name}`);
-  }
-
-  /**
-   * Creates a new module cache.
-   * @param {string} name - The name of the cache.
-   * @param {CacheOptions} [options={}] - The cache options.
-   * @param {boolean} [safe=true] - If `true`, a new cache will not be created
-   *   if it conflicts with another cache.
-   * @returns {Cache} The `Cache` that was created, or the existing cache with
-   *   this name if the `safe` option is enabled.
-   */
-  createCache(name, options = {}, safe = true) {
-    if (safe && this.hasCache(name)) {
-      return this.getCache(name);
-    }
-
-    return this.constructor.caches.create(`${this.cachePrefix}:${name}`, options);
+    this.name = name;
   }
 }
 
--- test/modules/ClientModuleTest.js
+++ test/modules/ClientModuleTest.js
@@ -13,69 +13,9 @@ describe('ClientModule', function() {
   describe('#constructor()', function() {
     const module = new ClientModule(client, 'test');
 
-    it('generates the proper cache prefix', function() {
-      expect(module.cachePrefix).to.equal('wot:test');
-    });
-  });
-
-  describe('#getCache()', function() {
-    const module = new ClientModule(client, 'test');
-    const cache = module.createCache('test');
-
-    it('gets existing caches', function() {
-      expect(module.getCache('test')).to.equal(cache);
-    });
-
-    it('returns undefined for non-existing caches', function() {
-      expect(module.getCache('fake')).to.be.undefined;
-    });
-  });
-
-  describe('#hasCache()', function() {
-    const module = new ClientModule(client, 'test');
-
-    module.createCache('test');
-
-    it('returns true for existing caches', function() {
-      expect(module.hasCache('test')).to.be.true;
-    });
-
-    it('returns false for non-existing caches', function() {
-      expect(module.hasCache('fake')).to.be.false;
-    });
-  });
-
-  describe('#createCache()', function() {
-    const module = new ClientModule(client, 'test');
-
-    it('creates a new cache', function() {
-      module.createCache('test');
-
-      expect(module.hasCache('test')).to.be.true;
-
-      module.createCache('testWithOptions', { cacheTTL: null });
-
-      expect(module.hasCache('testWithOptions')).to.be.true;
-    });
-
-    it('returns the cache created', function() {
-      const cache = module.createCache('test');
-
-      expect(module.getCache('test')).to.equal(cache);
-    });
-
-    it('does not overwrite existing caches if creation is safe', function() {
-      const cache = module.createCache('test');
-      const newCache = module.createCache('test');
-
-      expect(cache).to.equal(newCache);
-    });
-
-    it('overwrites existing caches if creation is unsafe', function() {
-      const cache = module.createCache('test');
-      const newCache = module.createCache('test', {}, false);
-
-      expect(cache).to.not.equal(newCache);
+    it('constructs the module correctly', function() {
+      expect(module.client).to.equal(client);
+      expect(module.name).to.equal('test');
     });
   });
 });
