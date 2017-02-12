0aaf90755f8298507c55f2dbfdcb089418c4c00f
Kelvin Wu <kelvinwu1996@gmail.com>
2017-02-12 12:30:58 -0500

    Reorganize mixins.

--- src/modules/mixins/Encyclopedia.js
+++ src/modules/mixins/Encyclopedia.js
@@ -19,7 +19,7 @@
  * @this {ClientModule}
  * @private
  */
-export default function encyclopediaSearch(params) {
+export const resolveEntry = function resolveEntry(params) {
   const {
     identifier,
     indexEndpoint,
@@ -59,4 +59,51 @@ export default function encyclopediaSearch(params) {
   }
 
   return Promise.reject(new TypeError('Expected a string or number as the entry identifier.'));
-}
+};
+
+/**
+ * Extracts the top modules of each type from a given module tree. The modules
+ *   with the highest experience cost are considered the 'top' modules.
+ * @param {Object} moduleTree - The module tree.
+ * @returns {Object} An object containing the top modules of each type found in
+ *   the module tree. The module types are the keys and the module data are the values.
+ * @private
+ */
+export const extractTopModules = function extractTopModules(moduleTree) {
+  return Object.keys(moduleTree).reduce((topModules, moduleId) => {
+    const module = moduleTree[moduleId];
+    const { price_xp, type } = module;
+
+    if (!topModules[type] || price_xp > topModules[type].price_xp) { // eslint-disable-line camelcase, max-len
+      return {
+        ...topModules,
+        [type]: module,
+      };
+    }
+
+    return topModules;
+  }, {});
+};
+
+/**
+ * Localizes a slug using values returned from an API endpoint.
+ * @param {Object} params - The function parameters.
+ * @param {string} params.method - The API method which returns the localization data.
+ * @param {string} params.type - The type of slug being localized.
+ * @param {string} params.slug - The slug being localized.
+ * @returns {Promise.<(string|undefined), Error>} Promise resolving to the
+ *   translated slug, or `undefined` if it couldn't be translated.
+ * @this {ClientModule}
+ * @private
+ */
+export const localize = function localize({ method, type, slug }) {
+  return this.client.get(method, {}).then((response) => {
+    const translations = response.data[type];
+
+    if (!translations || typeof translations !== 'object') {
+      throw new Error(`Invalid translation type: ${type}.`);
+    }
+
+    return translations[slug];
+  });
+};
